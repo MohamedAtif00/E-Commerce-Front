fe8529874ad891c4232cc2cbaa0d759e6fceb000
MohamedAtif00 <mohamed141462013@gmail.com>
2024-03-04 22:28:31 +0200

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { NavbarComponent } from './header/navbar/navbar.component';
+import { SearchComponent } from './header/search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    ProductListComponent,
+    ProductDetailsComponent,
+    ShoppingCartComponent,
+    NavbarComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
